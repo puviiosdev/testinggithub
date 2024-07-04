bddf3a70651079ff1752eb61b9b94e2ba489c647
puvi <puvi@puvis-MacBook-Pro.local>
2024-07-04 17:38:52 +0530

    text added

---
+++ testing/testing/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  testing
+//
+//  Created by puvi on 04/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ testing/testing/testingApp.swift
@@ -0,0 +1,17 @@
+//
+//  testingApp.swift
+//  testing
+//
+//  Created by puvi on 04/07/24.
+//
+
+import SwiftUI
+
+@main
+struct testingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
